b1736644b341b4d877903c4b0b1bd300354b25fe
Suhendra <amarsuhendra2@gmail.com>
2021-09-13 09:45:23 +0700

    tugas01
    tugas 01 lab PBO
    
    final ver

---
+++ tugas01/tugas01AmarSuhendra.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class tugas01AmarSuhendra {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+
+        //variabel
+        int Jbarang, Hbarang, Tbarang; 
+        float Diskon, Total;
+        
+        //meminta input jumlah barang
+        System.out.println("== Masukkan data anda ==");
+        System.out.print("Jumlah Barang :");
+        Jbarang = Input.nextInt();
+
+        //meminta input harga barang per unit
+        System.out.print("Harga per unit :");
+        Hbarang = Input.nextInt();
+        
+        //melakukan operasi
+        Tbarang = Jbarang*Hbarang;
+        Diskon = Tbarang*(15/100f); 
+        Total = Tbarang - Diskon;
+
+        //mencetak hasil
+        System.out.println("== Result ==");
+        System.out.println("Total Harga : "+ Total);
+        
+    }
+}
