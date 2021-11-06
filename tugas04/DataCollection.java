999ddd87787d7e4e82ff607f5fc9393788c2b84e
Suhendra <amarsuhendra2@gmail.com>
2021-11-06 22:18:39 +0700

    Update DataCollection.java

--- tugas04/DataCollection.java
+++ tugas04/DataCollection.java
@@ -1,9 +1,11 @@
-import java.util.Arrays;
+//import java.util.Arrays;
+//import java.util.Collection;
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 class Mahasiswa {
   String npm;
@@ -18,17 +20,18 @@ class Mahasiswa {
 }
 
 // class Buku {
-//   int id;
-//   String nama, pengarang, publisher;
-//   int jumlah;
+// int id;
+// String nama, pengarang, publisher;
+// int jumlah;
 
-//   public Buku(int id, String nama, String pengarang, String publisher, int jumlah) {
-//     this.id = id;
-//     this.nama = nama;
-//     this.pengarang = pengarang;
-//     this.publisher = publisher;
-//     this.jumlah = jumlah;
-//   }
+// public Buku(int id, String nama, String pengarang, String publisher, int
+// jumlah) {
+// this.id = id;
+// this.nama = nama;
+// this.pengarang = pengarang;
+// this.publisher = publisher;
+// this.jumlah = jumlah;
+// }
 // }
 
 public class DataCollection {
@@ -108,17 +111,48 @@ public class DataCollection {
     Mahasiswa mhs1 = new Mahasiswa("1608107010021", "Fulan 21", 2016);
     Mahasiswa mhs2 = new Mahasiswa("1608107010002", "Fulan 2", 2016);
     Mahasiswa mhs3 = new Mahasiswa("1608107010046", "Fulan 46", 2016);
+
     // Membuat ArrayList
     List<Mahasiswa> mahasiswa = new ArrayList<>();
     mahasiswa.add(mhs1);
     mahasiswa.add(mhs2);
     mahasiswa.add(mhs3);
     // Perulangan menggunakan Interface Iterator
-    Iterator itr = mahasiswa.iterator();
-    while (itr.hasNext()) {
-    Mahasiswa m = (Mahasiswa) itr.next();
-    System.out.println(m.npm + " " + m.nama + " " + m.angkatan);
+    // Iterator<Mahasiswa> itr = mahasiswa.iterator();
+    // while (itr.hasNext()) {
+    // Mahasiswa m = (Mahasiswa) itr.next();
+    // System.out.println(m.npm + " " + m.nama + " " + m.angkatan);
+    // }
+
+    List<Mahasiswa> mahasiswa1 = new ArrayList<>();
+
+    // = soal no 1: addAll()
+    Collections.addAll(mahasiswa1, mhs1, mhs2, mhs3);
+    Iterator<Mahasiswa> itr0 = mahasiswa.iterator();
+    while (itr0.hasNext()) {
+      Mahasiswa m = (Mahasiswa) itr0.next();
+      System.out.println(m.npm + " " + m.nama + " " + m.angkatan);
     }
+
+    // == soal no 2 : removeAll()
+    mahasiswa1.removeAll(mahasiswa);
+    Iterator<Mahasiswa> itr1 = mahasiswa.iterator();
+    while (itr1.hasNext()) {
+      Mahasiswa m = (Mahasiswa) itr1.next();
+      System.out.println(m.npm + " " + m.nama + " " + m.angkatan);
+    }
+
+    // === Soal no 3 : isEmpty()
+    System.out.println(mahasiswa1.isEmpty()); // true
+
+    // ==== Soal no 4 : 
+    System.out.println(mahasiswa1.size());
+
+    // ===== Soal no 5 : contains()
+    System.out.println(mahasiswa1.contains(2017));
+
+    //====== Soal no 6 : get()
+    System.out.println(mahasiswa.get(0));
     // ================ end of latihan 04
 
     // // ================ latihan 05
@@ -126,21 +160,25 @@ public class DataCollection {
     // Map<Integer, Buku> map = new HashMap<Integer, Buku>();
     // // Membuat objek Buku
 
-    // Buku b1 = new Buku(101, "Design Patterns: Elements of Reusable Object-Oriented Software", "Erich Gamma",
-    //     "Addison-Wesley Professional", 8);
-    // Buku b2 = new Buku(102, "Problem Frames: Analysing & Structuring Software Development Problems", "Michael Jackson",
-    //     "Addison-Wesley Professional", 4);
-    // Buku b3 = new Buku(103, "Software Architecture in Practice", "LenBass", "Addison-Wesley Professional", 6);
+    // Buku b1 = new Buku(101, "Design Patterns: Elements of Reusable
+    // Object-Oriented Software", "Erich Gamma",
+    // "Addison-Wesley Professional", 8);
+    // Buku b2 = new Buku(102, "Problem Frames: Analysing & Structuring Software
+    // Development Problems", "Michael Jackson",
+    // "Addison-Wesley Professional", 4);
+    // Buku b3 = new Buku(103, "Software Architecture in Practice", "LenBass",
+    // "Addison-Wesley Professional", 6);
     // // Menambahkan Buku ke map
     // map.put(1, b1);
     // map.put(2, b2);
     // map.put(3, b3);
     // // Proses Traversing map
     // for (Map.Entry<Integer, Buku> entry : map.entrySet()) {
-    //   int key = entry.getKey();
-    //   Buku b = entry.getValue();
-    //   System.out.println(key + " Details:");
-    //   System.out.println(b.id + " " + b.nama + " " + b.pengarang + "" + b.publisher + " " + b.jumlah);
+    // int key = entry.getKey();
+    // Buku b = entry.getValue();
+    // System.out.println(key + " Details:");
+    // System.out.println(b.id + " " + b.nama + " " + b.pengarang + "" + b.publisher
+    // + " " + b.jumlah);
     // }
     // // ================ end of latihan 05
   }
